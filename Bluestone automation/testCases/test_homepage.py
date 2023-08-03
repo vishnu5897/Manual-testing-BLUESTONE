7da6f976537af73a1d586092f673012490cb9f52
Varalakshmi115 <141239677+Varalakshmi115@users.noreply.github.com>
2023-08-03 20:10:43 +0530

    Update test_homepage.py

--- Bluestone automation/testCases/test_homepage.py
+++ Bluestone automation/testCases/test_homepage.py
@@ -1,1 +1,33 @@
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+# Replace 'your_browser_driver_path' with the path to your browser driver executable (e.g., chromedriver, geckodriver)
+driver = webdriver.Chrome(executable_path)
+driver.maximize_window()
+
+# Replace 'bluestone_website_url' with the actual URL of Bluestone's homepage
+website_url = 'https://www.bluestone.com/'
+driver.get(website_url)
+
+# Wait for the homepage to load
+wait = WebDriverWait(driver, 10)
+wait.until(EC.presence_of_element_located((By.ID, 'some_element_on_homepage')))
+
+# Perform actions on the homepage
+# For example, click on a menu item, perform a search, or interact with elements
+
+# Example: Click on the "Sign In" link (assuming it's an anchor element with an ID or class)
+sign_in_link = driver.find_element_by_id('sign-in-link')  # Replace 'sign-in-link' with the actual ID or class name
+sign_in_link.click()
+
+# Wait for the next page to load (e.g., the login page)
+wait.until(EC.presence_of_element_located((By.ID, 'login-form')))
+
+# Perform other actions as needed on the subsequent page (e.g., login)
+
+# Close the browser
+driver.quit()
